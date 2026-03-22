68205ee1485d850579bf6a7eb3b4a733793fcf97
Matan <matanew1@gmail.com>
2026-03-22 23:45:24 +0200

    `Removed SortedArray test code`

--- src/main.ts
+++ src/main.ts
@@ -83,14 +83,6 @@ async function bootstrap() {
     );
   }
 
-  const array = new SortedArray();
-  array.add(1);
-  array.add(2);
-  array.add(3);
-  array.add(4);
-  array.add(5);
-  array.add(6);
-
   // Graceful Shutdown
   app.enableShutdownHooks();
 
